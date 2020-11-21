ee18fff154029bef6e0a6407ee5851cda078eaf8
page <page@seventh77.com>
2020-11-21 08:54:31 +0800

    基础代码

---
+++ src/utils/data.js
@@ -0,0 +1,8 @@
+var Browser = Laya.Browser;
+var WebGL = Laya.WebGL;
+var Loader = Laya.Loader;
+var Stat = Laya.Stat;
+
+var BgPageX = 0, WindowW = 0;
+
+let BgmUrl = "https://cdn.dhteam.net/thermoFisher/audio/bgm.mp3";
---
+++ src/utils/resource.js
@@ -0,0 +1,9 @@
+var PreResources = [
+    { url: 'images/loading/1.png', type: Loader.IMAGE },
+    { url: 'res/atlas/images/out.atlas', type: Loader.ATLAS }
+];
+var Resources = [
+    { url: 'images/loading/1.png', type: Loader.IMAGE },
+    { url: 'res/atlas/images/out.atlas', type: Loader.ATLAS }
+];
+var unpackImg = [];
